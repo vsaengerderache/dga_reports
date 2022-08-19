5eac3191fc376d256c757ef61aa16ef819c449c6
vsaengerderache <vsaengerderache@gmail.com>
2022-08-19 14:43:37 -0400

    done code to sort data in columns

--- scripts/script1.R
+++ scripts/script1.R
@@ -1,9 +1,9 @@
-################################################################
-##Writen by Vicente Saenger Derache, Civil Engineering Student##                                                        ##
-##        Univerity of Concepción, Concepción, Chile          ##
-################################################################
+#################################################################
+##Written by Vicente Saenger Derache, Civil Engineering Student##                                                        ##
+##        University of Concepcion, Concepcion, Chile          ##
+#################################################################
 
-#### library                                                    ####
+#### library                                                     ####
 library(readr)
 library(dplyr)
 library(ggplot2)
@@ -13,56 +13,65 @@ library(writexl)
 library(lubridate)
 library(Hmisc)
 library(tidyr)
-#### entry data                                                 ####
+#### entry data                                                  ####
 df<- read_excel("data/08_2018_08_2021.xls", 
                 col_names = FALSE)
-#### extract data info                                          ####
-station       <-df[5,5]
-period        <-df[4,2]
-code_watershed<-df[4,7]
-watershed     <-df[4,8]
-sub_watershed <-df[4,9]
-#### seccion meses sin nombrar ####
-month_data    <-df[which(df$...1== 'MES:'), ]
-month_data    <-month_data[,3] 
-colnames(month_data)<-"month/year"
-#### delete useless info                                        ####
-df<-df[-c(seq(1,9)),-c(4,6,7,12,13,14,15,20,21)] #del col and rows
-#### column names                                               ####                                       
-colnames(df) <-rep(c(
-         "day",
-         "hour",
-         "height",
-         "flow"),3)
-#### sort data                                                  ####
+#### extract data info                                           ####
+station       <-df[6,4]
+period        <-df[5,1]
+code_watershed<-df[7,4]
+watershed     <-df[8,4]
+sub_watershed <-df[9,4]
+#### delete useless info                                         ####
+df<-df[-c(seq(1,9)),-c(4,6,7,12,13,14,15,20,21)] #delete col and rows
+#### rows with month data                                        ####
+row_month<-which(df$'...1' == 'MES:')
+#### vector process                                              ####
+vp<- append(row_month, nrow(df))
+#### process data                                                ####
+df_processed<-data.frame()
+for (i in 2:length(vp))
+{
+  #### extract data of one month                                 ####
+  a<-vp[i-1]                                 #auxiliary value
+  b<-vp[i]                                   #auxiliary value
+  month_data<-df[c(seq(a,b)),]               #data between months
+  #### extract month and year data                               ####
+  month_year<-data.frame(month_data[1,3])
+  colnames(month_year)<-("x")
+  month_year<-month_year %>% separate(x, c("month", "year"))
+  month<-as.numeric(month_year[1,1])
+  year<-as.numeric(month_year[1,2])
+  #### delete useless info                                       ####
+  month_data<-month_data[-c(seq(1,2)),]      #delete first 2 row
+  month_data<-month_data[-nrow(month_data),] #delete last row
+  month_data<-month_data[,-c(3,7,11)]        #delete height col
+  #### column names                                              ####
+  colnames(month_data) <-rep(c("day","hour","flow"),3)
+  #### sort data                                                 ####                                            
+  month_data<-rbind(month_data[1:3],
+            setNames(month_data[4:6], names(month_data)[1:3]),
+            setNames(month_data[7:9], names(month_data)[1:3]))     
+                                             #join data 
+
+  #### add month and year data                                   #### 
+  month_data$year<-year                      #add year col
+  month_data$month<-month                    #add month col
+  month_data<-month_data %>% separate(hour, c("hour", "minute"))  
+  month_data$day<-as.numeric(month_data$day) #numeric value day
+  month_data$hour<-as.numeric(month_data$hour)  
+                                             #numeric value hour
+  month_data$minute<-as.numeric(month_data$minute)  
+                                             #numeric value minute
+  month_data$flow<-signif(as.numeric(month_data$flow),digits = 2)
+  #### add date col                                              #### 
+  month_data$date<-with(month_data, 
+                        ymd_hm(paste(year, month, day, hour, minute, sep= ' ')))
+  month_data<-month_data[order(month_data$date),]
+  #### sort data ####
+  month_data<-month_data[,c(7,5,6,1,2,3,4)]    #sort 
+  #### merge months of data ####
+  df_processed<-merge(df_processed,month_data,all = TRUE)
+}
 
 
-
-#### trabajo anterior
-
-#### extraer mes y ano ####
-month_year          <-data.frame(df[10,3])
-colnames(month_year)<-("x")
-month_year          <-month_year %>% separate(x, c("month", "year"))
-month               <-as.numeric(month_year[1,1])
-year                <-as.numeric(month_year[1,2])
-
-#### ####
-
-
-df$year <-year                                    #a?ado columna a?o
-df$month<-month                                   ###a?ado columna mes
-df<-df[, c(4,5,1,2,3)]                            ###reordeno
-df<-df %>% separate(hour, c("hour", "minute"))    ###separo hora/minuto
-df<-df %>% separate(hour, c("hour", "minute"))    ###separo hora/minuto
-
-
-
-df<-df[, c(4,5, 1, 2, 3)]                         ###reordeno
-df<-df %>% separate(hour, c("hour", "minute"))    ###separo hora/minuto
-
-df$day<-as.numeric(df$day)                        ###valor numerico dia
-df$hour<-as.numeric(df$hour)                      ###valor numerico hora
-df<-df[, -5] 
-df$flow<-signif(as.numeric(df$flow))
-df<-df[-1,-c(3,7,11)]  
